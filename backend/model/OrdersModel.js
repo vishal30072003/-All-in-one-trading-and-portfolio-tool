aa7669b04709a02c55527ccc3a812f14ac0c8afe
vishal30072003 <bnag2413@gmail.com>
2025-07-24 16:05:54 +0530

    Delete backend directory

--- backend/model/OrdersModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-const { OrdersSchema } = require("../schemas/OrdersSchema");
-
-const OrdersModel = new model("order", OrdersSchema);
-
-module.exports = { OrdersModel };
